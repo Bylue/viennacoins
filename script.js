9d355384048dc5176d330143b070ebe0ab686697
bylue.org <98576933+Bylue@users.noreply.github.com>
2023-01-26 19:59:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function redirect1() {
+    window.location.href = "https://discord.gg/mxWgUFs32k";
+}
+document.getElementById("1").addEventListener("click", redirect);
+
+function redirect2() {
+    window.location.href = "https://tiktok.com/@viennacoins";
+}
+document.getElementById("2").addEventListener("click", redirect);
+
+function redirect2() {
+    window.location.href = "https://hypixel.net";
+}
+document.getElementById("2").addEventListener("click", redirect);
